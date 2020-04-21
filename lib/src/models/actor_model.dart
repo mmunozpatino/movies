1cec8ef7339cd8926b5fd2dd8c0b7b2fde28a85b
mmunozpatinomeli <mercedes.munozpatino@mercadolibre.com>
2020-04-21 20:27:34 -0300

    wip video 115

---
+++ lib/src/models/actor_model.dart
@@ -0,0 +1,20 @@
+class Actor {
+  int castId;
+  String character;
+  String creditId;
+  int gender;
+  int id;
+  String name;
+  int order;
+  String profilePath;
+
+  Actor({
+    this.character,
+    this.creditId,
+    this.gender,
+    this.id,
+    this.name,
+    this.order,
+    this.profilePath,
+  });
+}
