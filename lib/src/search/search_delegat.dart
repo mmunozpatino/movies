5f6385e1067903e02f1130b4650a053018eca54e
mmunozpatinomeli <mercedes.munozpatino@mercadolibre.com>
2020-06-21 21:00:37 -0300

    video 120 ok

---
+++ lib/src/search/search_delegat.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MovieSearch extends SearchDelegate {
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // Acciones del appBar! (icono para limpiar el texto por ejemplo)
+    
+    return null;
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // icono a la izq del appbar
+    return null;
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return null;
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return null;
+  }
+
+}
